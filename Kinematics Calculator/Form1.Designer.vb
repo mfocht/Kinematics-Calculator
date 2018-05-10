81fc45f5eda76a7dd4e5c3ccf207b1bd87900d80
Micah Focht <bandmfocht@gmail.com>
2018-05-10 10:22:29 -0400

    Some basic work.

--- Kinematics Calculator/Form1.Designer.vb
+++ Kinematics Calculator/Form1.Designer.vb
@@ -29,6 +29,25 @@ Partial Class Form1
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.timelabel = New System.Windows.Forms.Label()
+        Me.xinitiallabel = New System.Windows.Forms.Label()
+        Me.xlabel = New System.Windows.Forms.Label()
+        Me.accelerationlabel = New System.Windows.Forms.Label()
+        Me.ivelocitylabel = New System.Windows.Forms.Label()
+        Me.velocitylabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -101,11 +120,196 @@ Partial Class Form1
         Me.Label7.TabIndex = 6
         Me.Label7.Text = "Time"
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(149, 105)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(257, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(149, 134)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(257, 20)
+        Me.TextBox2.TabIndex = 8
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(149, 165)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(257, 20)
+        Me.TextBox3.TabIndex = 9
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(149, 194)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(257, 20)
+        Me.TextBox4.TabIndex = 10
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(149, 223)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(257, 20)
+        Me.TextBox5.TabIndex = 11
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(149, 252)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(257, 20)
+        Me.TextBox6.TabIndex = 12
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 291)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(776, 82)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(429, 252)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(43, 20)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "Time"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(429, 223)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(61, 20)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "X Initial"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(429, 194)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(20, 20)
+        Me.Label10.TabIndex = 17
+        Me.Label10.Text = "X"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(429, 165)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(97, 20)
+        Me.Label11.TabIndex = 16
+        Me.Label11.Text = "Acceleration"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(429, 134)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(105, 20)
+        Me.Label12.TabIndex = 15
+        Me.Label12.Text = "Initial Velocity"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(429, 105)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(64, 20)
+        Me.Label13.TabIndex = 14
+        Me.Label13.Text = "Velocity"
+        '
+        'timelabel
+        '
+        Me.timelabel.AutoSize = True
+        Me.timelabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.timelabel.Location = New System.Drawing.Point(592, 250)
+        Me.timelabel.Name = "timelabel"
+        Me.timelabel.Size = New System.Drawing.Size(0, 20)
+        Me.timelabel.TabIndex = 25
+        '
+        'xinitiallabel
+        '
+        Me.xinitiallabel.AutoSize = True
+        Me.xinitiallabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.xinitiallabel.Location = New System.Drawing.Point(592, 221)
+        Me.xinitiallabel.Name = "xinitiallabel"
+        Me.xinitiallabel.Size = New System.Drawing.Size(0, 20)
+        Me.xinitiallabel.TabIndex = 24
+        '
+        'xlabel
+        '
+        Me.xlabel.AutoSize = True
+        Me.xlabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.xlabel.Location = New System.Drawing.Point(592, 192)
+        Me.xlabel.Name = "xlabel"
+        Me.xlabel.Size = New System.Drawing.Size(0, 20)
+        Me.xlabel.TabIndex = 23
+        '
+        'accelerationlabel
+        '
+        Me.accelerationlabel.AutoSize = True
+        Me.accelerationlabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.accelerationlabel.Location = New System.Drawing.Point(592, 163)
+        Me.accelerationlabel.Name = "accelerationlabel"
+        Me.accelerationlabel.Size = New System.Drawing.Size(0, 20)
+        Me.accelerationlabel.TabIndex = 22
+        '
+        'ivelocitylabel
+        '
+        Me.ivelocitylabel.AutoSize = True
+        Me.ivelocitylabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ivelocitylabel.Location = New System.Drawing.Point(592, 132)
+        Me.ivelocitylabel.Name = "ivelocitylabel"
+        Me.ivelocitylabel.Size = New System.Drawing.Size(0, 20)
+        Me.ivelocitylabel.TabIndex = 21
+        '
+        'velocitylabel
+        '
+        Me.velocitylabel.AutoSize = True
+        Me.velocitylabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.velocitylabel.Location = New System.Drawing.Point(592, 103)
+        Me.velocitylabel.Name = "velocitylabel"
+        Me.velocitylabel.Size = New System.Drawing.Size(0, 20)
+        Me.velocitylabel.TabIndex = 20
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(800, 376)
+        Me.Controls.Add(Me.timelabel)
+        Me.Controls.Add(Me.xinitiallabel)
+        Me.Controls.Add(Me.xlabel)
+        Me.Controls.Add(Me.accelerationlabel)
+        Me.Controls.Add(Me.ivelocitylabel)
+        Me.Controls.Add(Me.velocitylabel)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
@@ -127,4 +331,23 @@ Partial Class Form1
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents timelabel As Label
+    Friend WithEvents xinitiallabel As Label
+    Friend WithEvents xlabel As Label
+    Friend WithEvents accelerationlabel As Label
+    Friend WithEvents ivelocitylabel As Label
+    Friend WithEvents velocitylabel As Label
 End Class
--- Kinematics Calculator/Form1.vb
+++ Kinematics Calculator/Form1.vb
@@ -1,3 +1,33 @@
 ﻿Public Class Form1
+    Dim active(5) As Boolean
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Checkboxes()
+        Calculate1d()
+    End Sub
+    Public Sub Checkboxes()
+        For i As Integer = 0 To 5
+            active(i) = False
+        Next
+        If TextBox1.Text <> "" And IsNumeric(TextBox1.Text) Then
+            active(0) = True
+        End If
+        If TextBox2.Text <> "" And IsNumeric(TextBox2.Text) Then
+            active(1) = True
+        End If
+        If TextBox3.Text <> "" And IsNumeric(TextBox3.Text) Then
+            active(2) = True
+        End If
+        If TextBox4.Text <> "" And IsNumeric(TextBox4.Text) Then
+            active(3) = True
+        End If
+        If TextBox5.Text <> "" And IsNumeric(TextBox5.Text) Then
+            active(4) = True
+        End If
+        If TextBox6.Text <> "" And IsNumeric(TextBox6.Text) Then
+            active(5) = True
+        End If
+    End Sub
+    Private Sub Calculate1d()
 
+    End Sub
 End Class
